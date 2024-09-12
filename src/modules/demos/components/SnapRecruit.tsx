26ed6d2612c9664708115eec2870b1ff369ca06a
Maheshwar Ugale <maheshwar.ugale@indexnine.com>
2024-09-12 10:03:18 +0530

    snap.recruit component data changes

--- src/modules/demos/components/SnapRecruit.tsx
+++ src/modules/demos/components/SnapRecruit.tsx
@@ -10,17 +10,17 @@ const SnapRecruit = () => {
       </p>
 
       <p>snap.<b>recruit</b> showcases our frontend and backend accelerator
-        projects which are part of <b>(snap.core). snap.core</b> contains
+        projects which are part of (snap.<b>core</b>). snap.<b>core</b> contains
         ready-made productized SaaS application modules for modern SaaS
         applications. These modules are tested and ready for use. Being
         pluggable, they can be turned on or off depending on need. However, the
-        advantage of using <b>snap.core</b> is a best-practice led SaaS
+        advantage of using snap.<b>core</b> is a best-practice led SaaS
         application out of the box which can greatly improve time-to-market.
       </p>
 
       <p>
         snap.<b>core</b> is available as a value add to all our customers
-        (either completely or module-wise). <b>snap.core</b> represents years of
+        (either completely or module-wise). snap.<b>core</b> represents years of
         experience building SaaS products that scale and more than 12 man months
         of engineering efforts.
       </p>
